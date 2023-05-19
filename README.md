f0cb0dd08b6ed3014b66431088b12f2950c268d6
Vincenzo117 <94491363+Vincenzo117@users.noreply.github.com>
2023-05-20 01:06:54 +0200

    markdown_parser

---
+++ README.md
@@ -0,0 +1,25 @@
+# Prompt Generator
+
+Prompt Generator is a flexible and user-friendly package that offers customizable scripts for generating meaningful and context-aware prompts. These prompts can be used to guide the writing, enhancement, and debugging of code.
+
+## Installation
+
+Prompt Generator can be installed easily using pip, a package installer for Python:
+
+```bash
+pip install prompt_generator
+```
+
+## Features
+
+- ### Markdown Parser
+
+  The Markdown Parser is a robust utility specifically designed to parse markdown files. It works by replacing all hyperlinks within a markdown document with the actual content of the linked files. This function proves extremely useful if you maintain a collection of prompt files written in markdown format, and these files contain references to other project documents.
+
+  To utilize the Markdown Parser, use the following command in your terminal:
+
+  ```bash
+  parse_md <path to your markdown file>
+  ```
+
+  Upon successful execution, the resultant parsed content will be displayed in your terminal. As an added convenience, this content is also automatically copied to your clipboard for immediate use or future reference.
---
+++ src/markdown_parser/script.py
@@ -0,0 +1,79 @@
+import os
+import re
+import pyperclip
+import argparse
+
+
+class TerminalColor:
+    """Class containing ANSI escape sequences for terminal colors and styles."""
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+def read_file(file_path):
+    """Reads a file and returns its content."""
+    try:
+        with open(file_path, 'r') as file:
+            return file.read()
+    except IOError:
+        print(f"{TerminalColor.WARNING}Failed to open {file_path}{TerminalColor.ENDC}")
+        return ""
+
+
+def parse_content(md_file_path):
+    """Parses a markdown file, replacing links with the content of the linked files."""
+    # Read the markdown file
+    content = read_file(md_file_path)
+
+    # Find all markdown links in the file
+    links = re.findall(r'\[(.*?)\]\((.*?)\)', content)
+
+    # Iterate over all links
+    for link in links:
+        text, file_path = link
+        full_file_path = os.path.realpath(os.path.join(
+            os.path.dirname(md_file_path), file_path))
+
+        # Check if file exists
+        if os.path.isfile(full_file_path):
+            file_content = read_file(full_file_path)
+            _, file_extension = os.path.splitext(full_file_path)
+
+            # If the linked file is a markdown file, parse it as well
+            if file_extension == '.md':
+                file_content = parse_content(full_file_path)
+
+            # Replace the markdown link with the file content in a code block
+            content = content.replace(
+                f'[{text}]({file_path})', f'```{file_extension[1:]}\n{file_content}\n```')
+
+    return content
+
+
+def main():
+    """Main function to parse markdown file."""
+    # Create ArgumentParser object
+    parser = argparse.ArgumentParser(description="Markdown File Parser")
+    parser.add_argument(
+        "md_file_path", help="path to the markdown file", type=str)
+    args = parser.parse_args()
+
+    # Use the function and print the result
+    result = parse_content(args.md_file_path)
+    print(f"\n{TerminalColor.HEADER}Parsed content:{TerminalColor.ENDC}\n")
+    print(result)
+
+    # Copy the result to the clipboard
+    pyperclip.copy(result)
+    print(f"\n{TerminalColor.OKBLUE}{TerminalColor.BOLD}Result has been copied to the clipboard!{TerminalColor.ENDC}\n")
+
+
+if __name__ == "__main__":
+    main()
